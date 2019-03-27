4230ff8b43517f3de464b9856ec47a4824cb93df
Brett Jory <bjory7@gmail.com>
2019-03-27 13:54:50 -0700

    checked solutions for problems 1-4. made minor changes to 1-3 to make it clearer that I had the correct solution. Corrected problems with 4, notes on issues are in the code.

--- assignment.sql
+++ assignment.sql
@@ -1,26 +1,29 @@
 -- 1. What query would you run to get the total revenue for March of 2012?
-select SUM(billing.amount)
+select extract(month from billing.charged_datetime) as "month", SUM(billing.amount) as revenue
 from billing
 where billing.charged_datetime >= '2012/03/01' and billing.charged_datetime < '2012/04/01';
 
 -- 2. What query would you run to get total revenue collected from the client with an id of 2?
-select concat(clients.first_name, ' ', clients.last_name) as client_name, sum(billing.amount) as revenue
+select clients.client_id, concat(clients.first_name, ' ', clients.last_name) as client_name, sum(billing.amount) as revenue
 from clients
 join billing on clients.client_id = billing.client_id and clients.client_id = 2
 group by client_name ;
 
 -- 3. What query would you run to get all the sites that client=10 owns?
-select concat(clients.first_name, ' ', clients.last_name) as client_name, sites.domain_name
+select clients.client_id, concat(clients.first_name, ' ', clients.last_name) as client_name, sites.domain_name
 from clients
 join sites on clients.client_id = sites.client_id and clients.client_id = 10;
 
 -- 4. What query would you run to get total # of sites created per month per year for the client with an id of 1? What about for client=20?
 	-- For client id 1
-select clients.client_id, count(sites.site_id) as sites_created, extract(month from sites.created_datetime) as "month", extract(year from sites.created_datetime) as "year"
+select clients.client_id, count(sites.site_id) as sites_created, month(sites.created_datetime) as "month", year(sites.created_datetime) as "year"
 from clients
 join sites on clients.client_id = sites.client_id and clients.client_id = 1
-group by extract(month from sites.created_datetime)
-order by sites.created_datetime asc;
+group by month(sites.created_datetime), year(sites.created_datetime)
+order by month(sites.created_datetime) asc;
+	-- got something wrong here, it is combining both sites created in November into one row even though they are in different years
+    -- error corrected, I was only grouping by month, not month and year
+
 	-- For client 20
 select clients.client_id, count(sites.site_id) as sites_created, extract(month from sites.created_datetime) as "month", extract(year from sites.created_datetime) as "year"
 from clients
