b60dcf768e06e9ef41b538d921cbbd9fe0259c0d
Brett Jory <bjory7@gmail.com>
2019-03-27 12:52:13 -0700

    queried total revenue collected from client id 2

--- assignment.sql
+++ assignment.sql
@@ -3,3 +3,8 @@ select SUM(billing.amount)
 from billing
 where billing.charged_datetime >= '2012/03/01' and billing.charged_datetime < '2012/04/01';
 
+-- 2. What query would you run to get total revenue collected from the client with an id of 2?
+select concat(clients.first_name, ' ', clients.last_name) as client_name, sum(billing.amount) as revenue
+from clients
+join billing on clients.client_id = billing.client_id and clients.client_id = 2
+group by client_name ;
